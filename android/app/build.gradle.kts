ee3bdf110fbb85e43f919a0f1a5949b7b74df58a
Gustavo Girão <56512641+Mufado@users.noreply.github.com>
2025-08-22 03:38:48 -0300

    Add Firebase

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,12 +1,17 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services")
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+dependencies {
+    implementation(platform("com.google.firebase:firebase-bom:34.1.0"))
+}
+
 android {
-    namespace = "com.example.pokedex_app"
+    namespace = "com.mufado.pokedex_app"
     compileSdk = 36
     ndkVersion = "27.0.12077973"
 
@@ -21,7 +26,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.pokedex_app"
+        applicationId = "com.mufado.pokedex_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 24
